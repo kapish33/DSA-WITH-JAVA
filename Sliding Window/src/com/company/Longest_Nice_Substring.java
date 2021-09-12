6f561616b6463de0405656ecf2cae34873992413
Kapish Singh <kapishsingh2209@gmail.com>
2021-09-12 15:36:20 +0530

    leetcode:E Maximum Average siing windows

--- Sliding Window/src/com/company/Longest_Nice_Substring.java
+++ Sliding Window/src/com/company/Longest_Nice_Substring.java
@@ -7,19 +7,16 @@ import java.util.Set;
 public class Longest_Nice_Substring {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println(longestNiceSubstring(sc.next()));
     }
-    public String longestNiceSubstring(String s) {
+    public static String longestNiceSubstring(String s) {
         Set<Character> charSet = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            charSet.add(s.charAt(i));
-        }//Making a set of unique characters
-        for (int i = 0; i < s.length(); i++) {
-            if (charSet.contains(Character.toUpperCase(s.charAt(i))) && charSet.contains(Character.toLowerCase(s.charAt(i)))) {
-                continue;
-            }
-            String s1 = longestNiceSubstring(s.substring(0, i));
+        for (int i = 0;i<s.length();i++) charSet.add(s.charAt(i)); // got all unique elements
+        for (int i = 0;i<s.length();i++){
+            if (charSet.contains(Character.toLowerCase(s.charAt(i))) && charSet.contains(Character.toUpperCase(s.charAt(i))))continue;
+            String s1= longestNiceSubstring(s.substring(0,i));
             String s2 = longestNiceSubstring(s.substring(i+1));
-            return s1.length()>= s2.length() ? s1 : s2; // returning longest substring
+            return s1.length()>=s2.length()? s1 : s2;
         }
         return s;
     }
