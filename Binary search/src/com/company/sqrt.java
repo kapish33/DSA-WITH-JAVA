21ba0cf4385e916690d30f29d3a82571fb3cae3c
Kapish Singh <kapishsingh2209@gmail.com>
2021-09-13 23:59:58 +0530

    Binary: Striver Live Serach ALL)

---
+++ Binary search/src/com/company/sqrt.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class sqrt {
+    public static void main(String[] args) {
+        int digit = 25;
+        System.out.println(sqrt(digit));
+    }
+    public static int sqrt(int n){
+        int low = 1,high = n/2,ans=1;
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if (mid*mid<=n) {
+                ans = mid;
+                low =mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+}
