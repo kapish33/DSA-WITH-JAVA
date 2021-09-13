1bf7c6716bd20fd9f6ae8661aa39a92039a8c659
Kapish Singh <kapishsingh2209@gmail.com>
2021-09-13 22:13:04 +0530

    leetcode:E binary

---
+++ Sliding Window/src/com/company/SlidingWindow_Maximum.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class SlidingWindow_Maximum {
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        System.out.println(maxSlidingWindow(nums,k));
+    }
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        for (int i = 0;i<k;i++){
+
+        }
+    }
+}
