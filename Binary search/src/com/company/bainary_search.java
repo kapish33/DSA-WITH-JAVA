ade75315344c46a9322e3e4a7bee447c162dced9
Kapish Singh <kapishsingh2209@gmail.com>
2021-09-13 22:16:00 +0530

    leetcode:E Binary Search Basic Made!

--- Binary search/src/com/company/bainary_search.java
+++ Binary search/src/com/company/bainary_search.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public class bainary_search {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(search(nums,target));
+    }
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + (end-start) / 2);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
