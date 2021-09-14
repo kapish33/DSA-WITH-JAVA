c293fc8273b6f0373fde8201e8801e7f56a4dc37
Kapish Singh <kapishsingh2209@gmail.com>
2021-09-14 22:34:19 +0530

    Binary: Striver : leetcode lowest in rotaed unique eelemnts+1

--- Binary search/src/com/company/rotaated_array.java
+++ Binary search/src/com/company/rotaated_array.java
@@ -3,7 +3,7 @@ package com.company;
 public class rotaated_array {
     public static void main(String[] args) {
         int[] array = {7,5,2,3,4};
-        System.out.println(lowest_in_rota(array,5));
+        System.out.println(lowest_in_rota(array,4));
     }
     public  static int lowest_in_rota(int[] array,int size){
         int low= 0,high = size;
