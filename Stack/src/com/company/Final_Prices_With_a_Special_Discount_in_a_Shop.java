c6f53e70dd78c0a7929ad58a5204678827cb6ff7
Kapish Singh <kapishsingh2209@gmail.com>
2021-09-20 12:31:38 +0530

    leetcode:E Stack Final Prices With a Special Discount in a Shop

---
+++ Stack/src/com/company/Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Final_Prices_With_a_Special_Discount_in_a_Shop {
+    public static void main(String[] args) {
+        int[] prices = {8,4,6,2,3};
+        int[] ans = finalPrices(prices);
+        for (int i =0;i< ans.length;i++) System.out.print(ans[i]+" ");
+    }
+    public static int[] finalPrices(int[] prices) {
+        //smallest left element
+        int[] ans = new int[prices.length];
+        ans[prices.length-1] = prices[prices.length-1];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(prices[prices.length-1]);
+        for (int i= prices.length-2; i>=0 ;i--){
+            while(!stack.isEmpty() && stack.peek()>prices[i])stack.pop();
+            ans[i]=stack.isEmpty()?prices[i]:prices[i]-stack.peek();
+            stack.push(prices[i]);
+        }
+        return ans;
+    }
+}
