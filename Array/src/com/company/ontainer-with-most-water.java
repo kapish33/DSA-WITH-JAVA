c79f91f0e13cbce3410c302665e312f7e41cb201
Kapish Singh <59023093+kapish33@users.noreply.github.com>
2021-09-26 20:20:25 +0530

    Create ontainer-with-most-water.java

---
+++ Array/src/com/company/ontainer-with-most-water.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r= height.length-1;
+        int max = 0;
+        while(l<r){
+            int right = height[r];
+            int left = height[l];
+            max = Math.max(max,Math.min(left,right)*(r-l));
+            if(left<right)l++;
+            else r--;  
+        }
+        return max;
+    }
+}
