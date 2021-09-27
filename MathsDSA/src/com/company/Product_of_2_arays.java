4d449e024b1a92ba33b90ec36468378c7ac98ab0
Kapish Singh <kapishsingh2209@gmail.com>
2021-09-27 21:14:07 +0530

    Maths: product Of Array+sumoffArr

--- MathsDSA/src/com/company/Product_of_2_arays.java
+++ MathsDSA/src/com/company/Product_of_2_arays.java
@@ -7,12 +7,14 @@ public class Product_of_2_arays {
         System.out.println(product(array1,array2));
     }
     public static int product(int[] arr1,int[] arr2){
-        int ans = 0;
-        for (int i = 0;i<arr1.length;i++){
-            for (int j = 0;j< arr2.length;j++){
-                ans=ans+arr1[i]*arr2[j];
-            }
-        }
-        return ans;
+        int suma=0,sumb=0,size1= arr1.length/2,size2= arr2.length/2;
+
+        for (int i = 0;i< size1;i++) suma=suma+arr1[i]+arr1[arr1.length-1-i];
+        if (arr1.length%2!=0) suma=suma+arr1[size1];
+
+        for (int i = 0;i< size2;i++) sumb=sumb+arr2[i]+arr2[arr2.length-1-i];
+        if (arr2.length%2!=0) sumb=sumb+arr1[size2];
+
+        return suma*sumb;
     }
 }
--- MathsDSA/src/com/company/addition_array.java
+++ MathsDSA/src/com/company/addition_array.java
@@ -7,8 +7,8 @@ public class addition_array {
     }
     public static int ans(int[] array){
         int sum = 0,size= array.length/2;
-        for (int i = 0;i< size;i++) sum=sum+array[i]+array[size*2-1];
-        if (array.length%2!=0) sum=sum+array[size+1];
+        for (int i = 0;i< size;i++) sum=sum+array[i]+array[array.length-i-1];
+        if (array.length%2!=0) sum=sum+array[size];
         return sum;
     }
 }
