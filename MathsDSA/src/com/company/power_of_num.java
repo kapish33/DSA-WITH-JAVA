68fdafd51d27a60185587404847604624860d12a
Kapish Singh <kapishsingh2209@gmail.com>
2021-09-27 22:39:35 +0530

    Maths: Power 2 ways itterative and recursive

---
+++ MathsDSA/src/com/company/power_of_num.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class power_of_num {
+    public static void main(String[] args) {
+        int num = 2;
+        int power= 11;
+        System.out.println(recoursePower(num,power));
+        System.out.println(iterativePower(num,power));
+    }
+    public static int recoursePower(int num, int power){
+        if(power==0) return 1;
+        int x = recoursePower(num,power/2);
+        if (power % 2 ==0) return x*x;
+        else return x*x*num;
+    }
+    public static int iterativePower(int a, int b){
+        int ans =1;
+        while (b>0){
+            if (b %2 ==1) ans= ans*a;
+            b=b/2;
+            a*=a;
+        }
+        return ans;
+    }
+}
