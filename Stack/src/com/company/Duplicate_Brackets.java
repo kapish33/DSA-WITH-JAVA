b4eb50e7a3d3dcf2a8a7ede6277b9bcfac3d8bca
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-05 08:57:26 +0530

    find Dublicates

---
+++ Stack/src/com/company/Duplicate_Brackets.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Duplicate_Brackets {
+    public static void main(String[] args) {
+        String sam = "(((a+b))+(b+c))";
+        System.out.println(dublicate_check(sam));
+    }
+    public static boolean dublicate_check(String sam){
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0;i<sam.length();i++){
+            char ch = sam.charAt(i);
+            if (ch == ')'){
+                if (st.peek() =='(') return true;
+                else {
+                    while (st.peek() != '(') st.pop();
+                    st.pop();
+                }
+            }else {
+                st.push(ch);
+            }
+        }return false;
+    }
+}
