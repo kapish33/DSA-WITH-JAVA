c8001273d8a1d7f30b81acc1c612c8da73f5c828
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-11 22:35:41 +0530

    stocksapn+1

--- test/src/com/company/stock_span.java
+++ test/src/com/company/stock_span.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.util.Stack;
+
 public class stock_span {
+    public static void main(String[] args) {
+        int[] stocks = {1,5,2,6,9,4,4,6,5,1,6,7,6,9,5,5,2};
+        int[] ans = stock_span(stocks);
+        for (int a:ans) System.out.print(a+" ");
+    }
+    public static int[] stock_span(int[] stocks){
+        int[] stocks_span = new int[stocks.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0;i< stocks.length;i++){
+            while (!stack.isEmpty() && stocks[stack.peek()]<stocks[i]) stack.pop();
+            stocks_span[i]=stack.isEmpty() ? i+1: i-stack.peek();
+            stack.push(i);
+        }
+        return stocks_span;
+    }
 }
