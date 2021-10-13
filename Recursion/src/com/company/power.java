2b5b37d51eeae8499fb27c2e8b23f634f79a160f
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-13 10:11:00 +0530

    recursion! 5

--- Recursion/src/com/company/power.java
+++ Recursion/src/com/company/power.java
@@ -1,2 +1,14 @@
-package com.company;public class power {
+package com.company;
+
+public class power {
+    public static void main(String[] args) {
+        int n = 5,pow = 0;
+        System.out.println(power(n,pow));
+    }
+    public  static int power(int n,int p){
+        if (p==0) return 1;
+        int xpm1 = power(n,p-1);
+        int xm = n * xpm1;
+        return xm;
+    }
 }
