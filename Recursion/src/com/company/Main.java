bf75325a2a30da2bdf9d0fd86df1c45625bf9cdf
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-15 22:23:31 +0530

    Test1

--- Recursion/src/com/company/Main.java
+++ Recursion/src/com/company/Main.java
@@ -1,8 +1,54 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
+class Main{
+    public static void main (String[] args){
 
-    public static void main(String[] args) {
-	// write your code here
+        Scanner sc=new Scanner(System.in);
+        int times=sc.nextInt();
+        for (int t=0;t<times;t++){
+            int num=sc.nextInt();
+            int [] arr=new int[num];
+            for (int i=0;i<num;i++)arr[i]=sc.nextInt();
+
+            int [][]stack=new int [num][2];
+            int [][]index=new int[num][2];
+            
+            int top=-1;
+            for (int j=0;j<num;j++){
+
+                while(top>-1 && stack[top][0]<arr[j])
+                {
+                    index[stack[top][1]][0]=arr[j];
+                    int dist=j-stack[top][1];
+                    index[stack[top][1]][1]=dist;
+                    top--;
+
+                }
+                stack[++top][0]=arr[j];
+                stack[top][1]=j;
+
+            }
+            while(top>-1){
+                index[stack[top][1]][0]=-1;
+                index[stack[top][1]][1]=-1;
+                top--;
+            }
+            for (int k=num-1;k>=0;k--){
+                while(top>-1 && stack[top][0]<arr[k]){
+                    int dist=stack[top][1]-k;
+                    if (dist<=index[stack[top][1]][1]||index[stack[top][1]][1]==-1){
+                        index[stack[top][1]][0]=arr[k];
+                    }
+                    top--;
+                }
+                stack[++top][0]=arr[k];
+                stack[top][1]=k;
+
+            }
+
+            for (int z=0;z<num;z++) System.out.print(index[z][0]+" ");
+            System.out.println();
+        }
     }
 }
--- Recursion/src/com/company/fllow_the_knight.java
+++ Recursion/src/com/company/fllow_the_knight.java
@@ -8,26 +8,26 @@ public class fllow_the_knight {
         Scanner sc = new Scanner(System.in);
 
         int i = sc.nextInt(),j = sc.nextInt(),n = sc.nextInt();
-        printKnightToor(n,i,j,board);
+        printKnightToor(n,i,j);
     }
-    public static void printKnightToor(int steps,int i ,int j,int[][] board){
+    public static void printKnightToor(int steps,int i ,int j){
         int count= 0;
         if (i<0 || i>9 || j<0 || j>9){
             if (steps ==0) {
                 board[i][j] = 1;
                 return;
             }
-            printKnightToor(steps-1,i-2,j+1,board);
-            printKnightToor(steps-1,i-2,j-1,board);
+            printKnightToor(steps-1,i-2,j+1);
+            printKnightToor(steps-1,i-2,j-1);
 
-            printKnightToor(steps-1,i-1,j+2,board);
-            printKnightToor(steps-1,i-1,j-2,board);
+            printKnightToor(steps-1,i-1,j+2);
+            printKnightToor(steps-1,i-1,j-2);
 
-            printKnightToor(steps-1,i+2,j+1,board);
-            printKnightToor(steps-1,i+2,j-1,board);
+            printKnightToor(steps-1,i+2,j+1);
+            printKnightToor(steps-1,i+2,j-1);
 
-            printKnightToor(steps-1,i+1,j-2,board);
-            printKnightToor(steps-1,i+1,j+2,board);
+            printKnightToor(steps-1,i+1,j-2);
+            printKnightToor(steps-1,i+1,j+2);
         }
 //        int count = 0;
         for (int p = 0;p<board.length;p++) for (int l  = 0;l< board.length;l++) if (board[p][l]==1) count++;
