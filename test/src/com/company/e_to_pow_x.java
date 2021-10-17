7ef57ccfe84e827c1a01ae1d7e71fe64864be659
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-17 21:08:36 +0530

    e to pw x

---
+++ test/src/com/company/e_to_pow_x.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class e_to_pow_x {
+    static double power = 1,fact = 1;
+    public static void main(String[] args) {
+        Scanner  sc= new Scanner(System.in);
+        System.out.format("%.4f",taylor(sc.nextInt(),sc.nextInt()));
+    }
+    public  static double taylor(int x,int n){
+        double rezz;
+        if (n==0) return 1;
+        rezz = taylor(x,n-1);
+        power = power * x;
+        fact = fact *n;
+        return (rezz +power /fact);
+    }
+}
