900f1aa55ae6a5f343b2d2a070727aad2a261ce3
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-20 20:57:39 +0530

    Rotate_the_array_easy_version

---
+++ Array/src/com/company/Rotate_the_array_easy_version.java
@@ -0,0 +1,32 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Rotate_the_array_easy_version {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int times= sc.nextInt();
+        for (int i = 0 ;i<times;i++){
+            int size = sc.nextInt();
+            int shift = sc.nextInt();
+            int new_shift = shift%size;
+            //entering array
+            int[] array = new int[size];
+            for (int j =0 ;j<size;j++){
+                array[j]= sc.nextInt();
+            }
+            for (int j =0 ;j<size;j++){
+//                System.out.println((j+shift)% (array.length-1));
+//                System.out.println(array[(j+shift)% (array.length-1)]+" ");
+                if (new_shift>0){
+                    System.out.print(array[(size+j-new_shift)% array.length]+" ");
+                }else {
+                    System.out.print(array[j]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
