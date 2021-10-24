d9830df4bdd3f81883f5ce05ab7e16b2cf699d54
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-24 19:24:11 +0530

    Get ALl subsequence+1

--- Recursion/src/com/company/geerate_all_subseq.java
+++ Recursion/src/com/company/geerate_all_subseq.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class geerate_all_subseq {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        subsequence("abc","",0);
+    }
+
+
+    public static void subsequence(String sam,String new_str,int curr) {
+        if (new_str.length()>0) System.out.println(new_str);
+        if (curr == sam.length()) return;
+        for (int i = curr; i < sam.length(); i++) {
+            new_str+=sam.charAt(i);
+            subsequence(sam,new_str,curr+1);
+            new_str.substring(new_str.length()-1);
+        }
+    }
 }
