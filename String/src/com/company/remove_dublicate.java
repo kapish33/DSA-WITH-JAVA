75d2e1015aaf353e47a331020a0e75efdd20ec68
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-24 19:30:39 +0530

    Remove Dublicate

---
+++ String/src/com/company/remove_dublicate.java
@@ -0,0 +1,15 @@
+package com.company;
+
+class remove_dublicate {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int i = 1, j = 0;
+        while (i < nums.length) {
+            if (nums[i] > nums[j]) {
+                nums[++j] = nums[i];
+            }
+            i++;
+        }
+        return j+1;
+    }
+}
