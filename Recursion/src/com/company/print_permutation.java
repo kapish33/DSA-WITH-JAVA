81f58b146f054c31a8742652808ffb6b9ebd9eb0
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-30 10:44:28 +0530

    Print Permuttions

--- Recursion/src/com/company/print_permutation.java
+++ Recursion/src/com/company/print_permutation.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class print_permutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        permutations(sc.next(),"");
+    }
+    public static void permutations(String sam,String ans){
+        if (sam.length()==0) {
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < sam.length(); i++) {
+            char ch = sam.charAt(i);
+            String ros = sam.substring(0,i)+sam.substring(i+1);
+            permutations(ros,ans+ch);
+        }
+    }
 }
