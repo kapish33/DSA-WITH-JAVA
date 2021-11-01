84f696689ac28394b8f049d4af52a9f5c3a2ea12
Kapish Singh <kapishsingh2209@gmail.com>
2021-11-01 21:52:45 +0530

    contains Dublicat

---
+++ Array/src/com/company/contaions_dublicate.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class contaions_dublicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size= sc.nextInt();
+        int[] array = new int[size];
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> ans = new HashSet<Integer>();
+        for (int num : nums) ans.add(num);
+        return nums.length - ans.size() != 0;
+    }
+}
