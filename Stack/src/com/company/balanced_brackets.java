4c5c53650dbd54e947b67ba500931dbe01902332
Kapish Singh <kapishsingh2209@gmail.com>
2021-11-01 19:27:51 +0530

    Again Balanced Para

---
+++ Stack/src/com/company/balanced_brackets.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class balanced_brackets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(checkBalance(sc.next()));
+    }
+    public static boolean checkBalance(String sam){
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < sam.length(); i++) {
+            char ch =  sam.charAt(i);
+            if (ch == ')'){
+                if (st.peek() == '(') return true;
+                else {
+                    while (st.peek()!='(') st.pop();
+                    st.pop();
+                }
+            }else {
+                st.push(ch);
+            }
+        }
+        return false;
+    }
+}
