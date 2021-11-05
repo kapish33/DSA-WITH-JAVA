1d4e76ac8ea387a01d8cc239c8f78562b2373ee2
Kapish Singh <kapishsingh2209@gmail.com>
2021-11-05 20:49:36 +0530

    linked list

--- Link_List/src/com/company/Main.java
+++ Link_List/src/com/company/Main.java
@@ -1,7 +1,18 @@
 package com.company;
 
+
 public class Main {
+    public  static class Node{
+        int data;
+        Node next;
+    }
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+    }
     public static void main(String[] args) {
-	// write your code here
+	// make a link list
+
     }
 }
---
+++ Stack/src/com/company/sliding_Window_maximum.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class sliding_Window_maximum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc
+    }
+}
