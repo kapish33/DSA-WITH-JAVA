5d321ba586bb4d7cfa19f5979beb690fb35ae358
Kapish Singh <kapishsingh2209@gmail.com>
2021-11-06 16:47:38 +0530

    St span

--- Stack/src/com/company/Stock_Span.java
+++ Stack/src/com/company/Stock_Span.java
@@ -8,18 +8,13 @@ public class Stock_Span {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int[] array = new int[size];
-        for (int i = 0; i <size ; i++) {
-            array[i]=sc.nextInt();
-        }
+        for (int i = 0; i <size ; i++) array[i]=sc.nextInt();
         int[] ans = stockSpan(array);
-        for (int i = 0; i < size; i++) {
-            System.out.print(ans[i]+" ");
-        }
+        for (int i = 0; i < size; i++) System.out.print(ans[i]+" ");
     }
     public static int[] stockSpan(int[] array){
         int[] ans = new int[array.length];
         Stack<Integer> st= new Stack<>();
-
         for (int i = 0; i < array.length; i++) {
             while (!st.isEmpty() && array[i] > array[st.peek()]) st.pop();
             ans[i]= st.isEmpty() ? i+1 : i-st.peek();
