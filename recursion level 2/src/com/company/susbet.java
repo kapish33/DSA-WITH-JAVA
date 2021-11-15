dd5322cd0b648a67796cea190438f62541b738c1
Kapish Singh <kapishsingh2209@gmail.com>
2021-11-15 09:51:12 +0530

    3

--- recursion level 2/src/com/company/susbet.java
+++ recursion level 2/src/com/company/susbet.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class susbet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int times = sc.nextInt();
+        for (int i = 0; i < times; i++) {
+            count_odds(sc.nextInt(),sc.nextInt());
+        }
+    }
+    public static int count_odds(int size,int odds){
+
+        //chose
+        return 1;
+    }
 }
