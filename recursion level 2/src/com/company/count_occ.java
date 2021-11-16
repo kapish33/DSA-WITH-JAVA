f11b16feb0396d7ff2429f244768e254f7e3c653
Kapish Singh <kapishsingh2209@gmail.com>
2021-11-16 21:50:33 +0530

    freq

--- recursion level 2/src/com/company/count_occ.java
+++ recursion level 2/src/com/company/count_occ.java
@@ -3,22 +3,15 @@ package com.company;
 import java.util.Scanner;
 
 public class count_occ {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
-
-        for (int i = 0; i < test; i++) {
-            int[] ans = new int[10];
-            int size = sc.nextInt();
-            int digit = sc.nextInt();
-            while (digit>0){
-                int rem =  digit%10;
-                ans[rem]++;
-                digit=digit/10;
-            }
-            for (int j = 0; j < 10; j++) {
-                System.out.print(ans[9-j]+" ");
-            }
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        for (int j=0;j<T;j++){
+            int N=sc.nextInt();
+            String str =sc.next();
+            int [] arr=new int[10];
+            for (int k=0;k<N;k++) arr[str.charAt(k)-'0']+=1;// solution
+            for (int i=0;i<10;i++)System.out.print(arr[9-i]+" ");// printing
             System.out.println();
         }
     }
