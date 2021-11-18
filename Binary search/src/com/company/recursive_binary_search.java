64b80ac8b909e5f8e79501658850df42d331f6e1
Kapish Singh <kapishsingh2209@gmail.com>
2021-11-18 11:51:19 +0530

    mas2

--- Binary search/src/com/company/recursive_binary_search.java
+++ Binary search/src/com/company/recursive_binary_search.java
@@ -6,14 +6,14 @@ public class recursive_binary_search {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int[] array = new int[size];
         int target = sc.nextInt();
+        int[] array = new int[size];
         for (int i = 0; i < size; i++) array[i]= sc.nextInt();
-        System.out.println(binarySearch(array,target,0, size));
+        System.out.println(binarySearch(array,0, size-1,target));
     }
     public static int binarySearch(int[] nums, int left, int right, int target) {
-        if (left < right) return -1;
-        int mid = left + (right - left) / 2;
+        if (left > right) return -1;
+        int mid = (left + right) / 2;
         if (target == nums[mid]) return 1;
         else if (target < nums[mid]) return binarySearch(nums, left, mid - 1, target);
         else return binarySearch(nums, mid + 1, right, target);
