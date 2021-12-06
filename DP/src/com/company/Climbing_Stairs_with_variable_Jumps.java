b9f32af07763be8b7bd3164c91b887d20fbb596c
Kapish Singh <kapishsingh2209@gmail.com>
2021-12-06 23:24:12 +0530

    ssa

---
+++ DP/src/com/company/Climbing_Stairs_with_variable_Jumps.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Climbing_Stairs_with_variable_Jumps {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) array[i]=sc.nextInt();
+        int[] dp = new int[size+1];
+        dp[size]=1;
+        for (int i = size-1; i >=0 ; i--) {
+            for (int j = 1; j < array[i] && i+j< dp.length; j++) {
+                dp[i]+=dp[i+j];
+            }
+
+        }
+        System.out.println(dp[0]);
+    }
+
+}
