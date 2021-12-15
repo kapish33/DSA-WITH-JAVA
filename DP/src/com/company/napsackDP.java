91945f9e8567699b65535bb828ac523760f26558
Kapish Singh <kapishsingh2209@gmail.com>
2021-12-15 22:27:03 +0530

    ms

---
+++ DP/src/com/company/napsackDP.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class napsackDP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] weight = new int[size];
+        int[] items = new int[size];
+    }
+}
