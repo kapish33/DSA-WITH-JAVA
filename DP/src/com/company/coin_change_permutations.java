3deb1106af1b04dae6cdd2b0b5c3588ddee92dc5
Kapish Singh <kapishsingh2209@gmail.com>
2021-12-16 23:34:31 +0530

    updtae

--- DP/src/com/company/coin_change_permutations.java
+++ DP/src/com/company/coin_change_permutations.java
@@ -9,9 +9,18 @@ public class coin_change_permutations {
         int[] array = new int[size];
         for (int i = 0; i < size; i++) array[i]=sc.nextInt();
         int targer = sc.nextInt();
-        System.out.println(coin_change_permut(array,size,targer));
+        System.out.println(coin_change_permut(array,size,targer,new int[targer+1]));
     }
-    public static int coin_change_permut(int[] array,int size ,int target){
-        return 12;
+    public static int coin_change_permut(int[] coins, int size, int target, int[] dp){
+        dp[0]=1;
+        for (int amt = 1; amt < target+1; amt++) {
+            for (int coin:coins) {
+                if (coin<=amt){
+                    int rem = amt-coin;
+                    dp[amt]+=dp[rem];
+                }
+            }
+        }
+        return dp[target];
     }
 }
