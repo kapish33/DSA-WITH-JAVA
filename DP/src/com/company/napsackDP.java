3c975d0d2be06a8a60e385003ad252800223e368
Kapish Singh <kapishsingh2209@gmail.com>
2021-12-17 22:14:06 +0530

    0 1 napsac

--- DP/src/com/company/napsackDP.java
+++ DP/src/com/company/napsackDP.java
@@ -6,16 +6,20 @@ public class napsackDP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size =5;// sc.nextInt();
-        int[] weight = {1,2,3,2,2};//new int[size];
-        int[] items = {8,4,0,5,34};//new int[size];
-        int capacity =4;// sc.nextInt();
-        System.out.println(zeroOneNapSac(items,weight,capacity,size));
+        int[] weight = {2,5,1,3,4};//new int[size];
+        int[] val = {15,14,10,45,30};//new int[size];
+        int capacity =7;// sc.nextInt();
+        int[][] dp = new int[size+1][capacity+1];
+        System.out.println(zeroOneNapSac(weight,val,capacity,size,dp));
     }
-    public static int zeroOneNapSac(int[] weight,int[] value,int cap,int size){
-        if (size==0 || cap ==0) return 0;
-        if (value[size-1]<=cap){
-            return Math.max(value[size-1],Math.max(zeroOneNapSac(weight, value, cap-weight[size-1], size-1),zeroOneNapSac(weight, value, cap, size-1)) );
+    public static int zeroOneNapSac(int[] weight,int[] items,int cap,int size,int[][] dp){
+        for (int i = 1; i < size+1; i++) {
+            for(int j = 1; j < cap+1; j++){
+                int val = items[i - 1];
+                int wt = weight[i - 1];
+                dp[i][j]= j>=wt ? Math.max(dp[i - 1][j], dp[i - 1][j - wt] + val) :dp[i - 1][j];
+            }
         }
-        return zeroOneNapSac(weight, value, cap, size-1);
+        return dp[size][cap];
     }
 }
