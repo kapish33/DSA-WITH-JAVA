662fa32477897c2f7febae1950546b3c0ab8e8dd
Kapish Singh <kapishsingh2209@gmail.com>
2021-12-18 22:47:30 +0530

    message

---
+++ DP/src/com/company/unboundedKnapSack.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class unboundedKnapSack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
