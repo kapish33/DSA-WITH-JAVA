4bc638caede7b57a291badafc7c65ed2de8bac21
Kapish Singh <kapishsingh2209@gmail.com>
2021-12-27 11:28:11 +0530

    message

---
+++ DP/src/com/company/test.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int stand = sc.nextInt();
+        int dest = sc.nextInt();
+        // passenger 1 ,express =2
+
+    }
+}
