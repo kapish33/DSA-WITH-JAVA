3984d03808a0c7026d621ae78158441ffa92d31d
Kapish Singh <kapishsingh2209@gmail.com>
2021-12-28 12:18:31 +0530

    LinkedList display and size

---
+++ Link_List/src/com/company/displayAlinkedList.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class displayAlinkedList {
+    public static class  Node{
+        int data;
+        Node next;
+    }
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+
+            size++;
+        }
+
+        public int size() {
+            // write code here
+            return size;
+        }
+
+        public void display() {
+            // write code here
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+}
