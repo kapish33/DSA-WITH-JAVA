451f19bdd5482929bfc4e190b77c1111da9a8402
Kapish Singh <kapishsingh2209@gmail.com>
2022-03-21 22:33:36 +0530

    aading

---
+++ DP/src/com/company/master.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class master {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
